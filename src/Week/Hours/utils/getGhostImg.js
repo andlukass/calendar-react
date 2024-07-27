ba2948d6a8ad8e7265644d62dd7b39edf0242e52
andlukass <lopess.andrade@gmail.com>
2024-07-27 20:44:19 +0100

    fix scale issues

--- src/Week/Hours/utils/getGhostImg.js
+++ src/Week/Hours/utils/getGhostImg.js
@@ -2,26 +2,19 @@ import html2canvas from "html2canvas";
 
 export const getGhostImg = async (ref, imgRef) => {
   if (ref.current) {
+    const width = ref.current.offsetWidth;
+    const height = ref.current.offsetHeight;
     const canvas = await html2canvas(ref.current, {
       backgroundColor: null, // Define o fundo como transparente
+      scale: 1,
+      width: width,
+      height: height,
     });
     const imgData = canvas.toDataURL('image/png');
     const img = new Image();
     img.src = imgData;
     img.onload = () => {
-      const halfWidth = img.width / 2;
-      const halfHeight = img.height / 2;
-
-      const resizedCanvas = document.createElement('canvas');
-      resizedCanvas.width = halfWidth;
-      resizedCanvas.height = halfHeight;
-
-      const ctx = resizedCanvas.getContext('2d');
-      ctx.drawImage(img, 0, 0, halfWidth, halfHeight);
-
-      const resizedImg = new Image();
-      resizedImg.src = resizedCanvas.toDataURL('image/png');
-      imgRef.current = (resizedImg);
+      imgRef.current = (img);
     };
   }
 };
