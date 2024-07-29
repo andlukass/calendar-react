76dd52be22da94b0da126105b55ff078e79a796a
andlukass <lopess.andrade@gmail.com>
2024-07-29 14:28:27 +0100

    rm endday

--- src/data/events/initialEvents.js
+++ src/data/events/initialEvents.js
@@ -2,7 +2,6 @@ export const initialEvents = [
     {
       id: 53,
       date: new Date("07/27/2024"),
-      endDay: new Date("07/27/2024"),
       start: 2,
       end: 3,
       user: "1",
@@ -11,7 +10,6 @@ export const initialEvents = [
     {
       id: 63,
       date: new Date("07/27/2024"),
-      endDay: new Date("07/27/2024"),
       start: 22,
       end: 26,
       user: "2",
@@ -20,9 +18,8 @@ export const initialEvents = [
     {
       id: 4,
       date: new Date(),
-      endDay: new Date(),
-      start: -1,
-      end: -1,
+      start: 5,
+      end: 8,
       user: "2",
       title: "b",
     },
